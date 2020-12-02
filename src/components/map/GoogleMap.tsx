b9f0e9de7d89c98b716b36e3ef9020e59f10eb83
sunyang9876 <sunyang9876@hotmail.com>
2020-12-02 12:36:15 +1000

    add useEffect

--- src/components/map/GoogleMap.tsx
+++ src/components/map/GoogleMap.tsx
@@ -45,7 +45,7 @@ const MyGoogleMap = ({notes}:any) => {
   return (
     <div style={{ height: '90vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        bootstrapURLKeys={{ key: "AIzaSyAjR3MxQwnBjdzcORF0tEy9OTwsxOz-p0Y" }}
         defaultCenter={defaultMapSetting.center}
         defaultZoom={defaultMapSetting.zoom}
         center={location}
